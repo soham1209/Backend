40cb6f88dbfd29db48bdfb7641800c26172abbf4
soham1209 <sohamns1234@gmail.com>
2025-10-04 16:25:27 +0530

    feat: add models for Comment, Like, Playlist, and Tweet with schema definitions

---
+++ src/models/like.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+        //  required: true
+    },
+    comment:{
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+        // required:true
+    },
+    tweet:{
+        type: Schema.Types.ObjectId,
+        ref: "Tweet",
+        // required:true
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      // required:true
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = model("Like", likeSchema);
